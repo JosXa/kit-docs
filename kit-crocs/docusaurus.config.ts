4592fb7dc52c7c83155a991d295eca933c113218
Joscha Götzer <joscha.goetzer@gmail.com>
2024-05-09 01:16:00 +0200

    Prepare GitHub pages

--- kit-crocs/docusaurus.config.ts
+++ kit-crocs/docusaurus.config.ts
@@ -8,15 +8,18 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://josxa.github.io/docusaurus/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'johnlindquist', // Usually your GitHub org/user name.
+  organizationName: 'josxa', // Usually your GitHub org/user name.
   projectName: 'kit-docs', // Usually your repo name.
+
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
